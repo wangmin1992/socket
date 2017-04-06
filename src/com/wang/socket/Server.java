c3f7b579e1f203d7970436e1d5f18c8ebe4b9238
wangmin1992 <wangmin992@outlook.com>
2017-04-06 17:24:12 +0800

    socket
    Java网络编程TCP/IP实例，服务端逻辑代码

--- src/com/wang/socket/Server.java
+++ src/com/wang/socket/Server.java
@@ -1,5 +1,8 @@
 package com.wang.socket;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * TCP Socket服务端。
  * @author wangmin
@@ -8,4 +11,25 @@ package com.wang.socket;
  */
 public class Server {
 
+	public static void main(String[] args) {
+		try 
+		{
+			//服务端在15000端口监听客户端TCP的连接。
+			ServerSocket serverSocket = new ServerSocket(15000);
+			Socket client = null;
+			boolean flag = true;
+			while (flag)
+			{
+				//等待客户端的连接 
+				client = serverSocket.accept();
+				System.out.println("与客户端连接成功");
+				new Thread(new ServerThread(client)).start();;
+			}
+			serverSocket.close();
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
