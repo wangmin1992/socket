2882dd4d77b58cd9957bcd08adb1bd5a461c4ff6
wangmin1992 <wangmin992@outlook.com>
2017-04-06 16:28:59 +0800

    socket
    服务端需要用到多线程，该类实现逻辑代码。

--- src/com/wang/socket/ServerThread.java
+++ src/com/wang/socket/ServerThread.java
@@ -1,5 +1,8 @@
 package com.wang.socket;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 
 /**
@@ -12,8 +15,54 @@ public class ServerThread implements Runnable{
 
 	private Socket client = null;
 	
+	/**
+	 * 构造函数。
+	 * @param client
+	 */
+	public ServerThread(Socket client)
+	{
+		this.client = client;
+	}
+	
+	
 	@Override
-	public void run() {
+	public void run() 
+	{
+		try 
+		{
+			//获得Socket输出流，用于向客户端发送数据。
+			PrintStream out = new PrintStream(client.getOutputStream());
+			//获得Socket输入流，用于接收客户端发送过来的数据。
+			BufferedReader buf = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			boolean flag = true;
+			while(flag)
+			{
+				//接收从客户端发送过来的数据。
+				String str = buf.readLine();
+				if (str == null && "".equals(str))
+				{
+					flag = false;
+				}
+				else
+				{
+					if ("bye".equals(str))
+					{
+						flag = false;
+					}
+					else
+					{
+						//将接收到的数据拼接上“服务端接收的数据为：”，然后再发送给客户端
+						out.println("服务端接收的数据为：" + str);
+					}
+				}
+			}
+			out.close();
+			client.close();
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 		
 	}
 
